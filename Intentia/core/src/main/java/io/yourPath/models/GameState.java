33f889330b6ef933c394ec85900f4a5573f2bb2b
JoseEstevez520 <joseestevezdaw@gmail.com>
2026-04-13 23:06:11 +0200

    feat: GameState creado, archivo para el guardado. Historia corregida en el json

--- Intentia/core/src/main/java/io/yourPath/models/GameState.java
+++ Intentia/core/src/main/java/io/yourPath/models/GameState.java
@@ -1,4 +1,37 @@
 package io.yourPath.models;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GameState {
+    private Set<String> flags;
+    private String currentNodeId;
+
+    public GameState() {
+        this.flags = new HashSet<>();
+    }
+
+    public boolean hasFlag(String flag) {
+        return flags.contains(flag);
+    }
+
+    public void addFlag(String flag) {
+        flags.add(flag);
+    }
+
+    public Set<String> getFlags() {
+        return flags;
+    }
+
+    public void setFlags(Set<String> flags) {
+        this.flags = flags;
+    }
+
+    public String getCurrentNodeId() {
+        return currentNodeId;
+    }
+
+    public void setCurrentNodeId(String currentNodeId) {
+        this.currentNodeId = currentNodeId;
+    }
 }
